ced2935d03de0c63c2e59da94b682586255ba398
Nick Block <nicholasblock78@gmail.com>
2017-01-20 18:39:12 -0600

    Fix sorting bug. All tests passing for first function

--- src/assignment-1.js
+++ src/assignment-1.js
@@ -4,8 +4,8 @@ var MyArray = function(array) {
 }
 
 MyArray.prototype.greatestDifference = function() {
-  var sortedArr = this.arrInput.sort();
+  var sortedArr = this.arrInput.sort(function(a,b){return a-b;});
   var endIndex = sortedArr.length - 1;
 
-  return sortedArr[endIndex] - sortedArr[0]
+  return sortedArr[endIndex] - sortedArr[0];
 }
