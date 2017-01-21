0931cecbf964fd6134cc80f0019f8e2de397a86b
Nick Block <nicholasblock78@gmail.com>
2017-01-20 18:57:17 -0600

    All tests passing with refactor needed

--- spec/assignment-1-spec.js
+++ spec/assignment-1-spec.js
@@ -3,44 +3,44 @@ describe("Assignment 1", function() {
   describe("Assignment 1 - first function", function() {
     it("Greatest Difference of Array 1", function() {
       array = new FirstArray([1,4,2,8]);
-      expect(array.greatestDifference()).toEqual(7);
+      expect(array.greatestDifference1()).toEqual(7);
     });
 
     it("Greatest Difference of Array 2", function() {
       array = new FirstArray([0,9,1,3,7]);
-      expect(array.greatestDifference()).toEqual(9);
+      expect(array.greatestDifference1()).toEqual(9);
     });
 
     it("Greatest Difference of Array 3 - triple digit numbers", function() {
       array = new FirstArray([88,99,100]);
-      expect(array.greatestDifference()).toEqual(12);
+      expect(array.greatestDifference1()).toEqual(12);
     });
 
     it("Greatest Difference of Array 4 - negative numbers", function() {
       array = new FirstArray([0,3,-5]);
-      expect(array.greatestDifference()).toEqual(8);
+      expect(array.greatestDifference1()).toEqual(8);
     });
   });
 
   describe("Assignment 1 - second function", function() {
     it("Greatest Difference of Array 1", function() {
       array = new SecondArray([1,4,2,8]);
-      expect(array.greatestDifference()).toEqual(7);
+      expect(array.greatestDifference2()).toEqual(7);
     });
 
     it("Greatest Difference of Array 2", function() {
       array = new SecondArray([0,9,1,3,7]);
-      expect(array.greatestDifference()).toEqual(9);
+      expect(array.greatestDifference2()).toEqual(9);
     });
 
     it("Greatest Difference of Array 3 - triple digit numbers", function() {
       array = new SecondArray([88,99,100]);
-      expect(array.greatestDifference()).toEqual(12);
+      expect(array.greatestDifference2()).toEqual(12);
     });
 
     it("Greatest Difference of Array 4 - negative numbers", function() {
       array = new SecondArray([0,3,-5]);
-      expect(array.greatestDifference()).toEqual(8);
+      expect(array.greatestDifference2()).toEqual(8);
     });
   });
 });
--- src/assignment-1.js
+++ src/assignment-1.js
@@ -3,7 +3,7 @@ var FirstArray = function(array) {
   this.arrInput = array;
 }
 
-FirstArray.prototype.greatestDifference = function() {
+FirstArray.prototype.greatestDifference1 = function() {
   var sortedArr = this.arrInput.sort(function(a,b){return a-b;});
   var endIndex = sortedArr.length - 1;
 
@@ -12,8 +12,24 @@ FirstArray.prototype.greatestDifference = function() {
 
 var SecondArray = function(array) {
   this.arrInput = array;
+  this.smallestInt;
+  this.largestInt;
 }
 
-SecondArray.prototype.greatestDifference = function() {
+SecondArray.prototype.greatestDifference2 = function() {
+  this.smallestInt = this.arrInput[0];
+  this.largestInt = this.arrInput[1];
 
+  for(var i=0; i < this.arrInput.length; i++) {
+    if(this.arrInput[i] < this.smallestInt) {
+      this.smallestInt = this.arrInput[i];
+    }
+    if(this.arrInput[i] > this.largestInt) {
+      this.largestInt = this.arrInput[i];
+    }
+  }
+  return this.largestInt - this.smallestInt;
 }
+
+//What is their difference in Big-O? Check bigocheatsheet.com to find out
+ 
