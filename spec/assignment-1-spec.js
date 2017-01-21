d49d72fb68371a8f1542708cdff40d09c41e898b
Nick Block <nicholasblock78@gmail.com>
2017-01-20 18:45:02 -0600

    Refactor for second way/function. Second set of tests all failing

--- spec/assignment-1-spec.js
+++ spec/assignment-1-spec.js
@@ -1,23 +1,46 @@
 
 describe("Assignment 1", function() {
+  describe("Assignment 1 - first function", function() {
+    it("Greatest Difference of Array 1", function() {
+      array = new FirstArray([1,4,2,8]);
+      expect(array.greatestDifference()).toEqual(7);
+    });
 
-  it("Greatest Difference of Array 1", function() {
-    array = new MyArray([1,4,2,8]);
-    expect(array.greatestDifference()).toEqual(7);
+    it("Greatest Difference of Array 2", function() {
+      array = new FirstArray([0,9,1,3,7]);
+      expect(array.greatestDifference()).toEqual(9);
+    });
+
+    it("Greatest Difference of Array 3 - triple digit numbers", function() {
+      array = new FirstArray([88,99,100]);
+      expect(array.greatestDifference()).toEqual(12);
+    });
+
+    it("Greatest Difference of Array 4 - negative numbers", function() {
+      array = new FirstArray([0,3,-5]);
+      expect(array.greatestDifference()).toEqual(8);
+    });
   });
 
-  it("Greatest Difference of Array 2", function() {
-    array = new MyArray([0,9,1,3,7]);
-    expect(array.greatestDifference()).toEqual(9);
-  });
+  describe("Assignment 1 - second function", function() {
+    it("Greatest Difference of Array 1", function() {
+      array = new SecondArray([1,4,2,8]);
+      expect(array.greatestDifference()).toEqual(7);
+    });
 
-  it("Greatest Difference of Array 3 - triple digit numbers", function() {
-    array = new MyArray([88,99,100]);
-    expect(array.greatestDifference()).toEqual(12);
-  });
+    it("Greatest Difference of Array 2", function() {
+      array = new SecondArray([0,9,1,3,7]);
+      expect(array.greatestDifference()).toEqual(9);
+    });
 
-  it("Greatest Difference of Array 4 - negative numbers", function() {
-    array = new MyArray([0,3,-5]);
-    expect(array.greatestDifference()).toEqual(8);
+    it("Greatest Difference of Array 3 - triple digit numbers", function() {
+      array = new SecondArray([88,99,100]);
+      expect(array.greatestDifference()).toEqual(12);
+    });
+
+    it("Greatest Difference of Array 4 - negative numbers", function() {
+      array = new SecondArray([0,3,-5]);
+      expect(array.greatestDifference()).toEqual(8);
+    });
   });
 });
--- src/assignment-1.js
+++ src/assignment-1.js
@@ -1,11 +1,19 @@
 
-var MyArray = function(array) {
+var FirstArray = function(array) {
   this.arrInput = array;
 }
 
-MyArray.prototype.greatestDifference = function() {
+FirstArray.prototype.greatestDifference = function() {
   var sortedArr = this.arrInput.sort(function(a,b){return a-b;});
   var endIndex = sortedArr.length - 1;
 
   return sortedArr[endIndex] - sortedArr[0];
+}
+
+var SecondArray = function(array) {
+  this.arrInput = array;
+}
+
+SecondArray.prototype.greatestDifference = function() {
+
 }
